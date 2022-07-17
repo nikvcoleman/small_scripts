e237e504e24a79ca646da396f93a99017f76e18c
Nikolas Coleman <nikvcoleman@gmail.com>
2022-07-17 13:37:39 -0400

    Update Base_rhel8_script.sh
    updated script to: 
    append 'neofetch' to .bashrc file using tee command
    got rid of 'keepassxc' installation 
    changed where 'make' and 'automake' were installed

--- RHEL8/Base_rhel8_script.sh
+++ RHEL8/Base_rhel8_script.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
+#THE FOLLOWING SCRIPT WILL RUN ON RHEL8 GIVEN THE USB IS NAMED: rhelscripts AND THE USERNAME IS: nik
 clear
-# Install Make
-echo ...Installing "make"
-sudo yum install make -y
-# Install automake if not already installed
-echo ...Installing "automake"
-sudo yum install automake -y
 #check to see if file exists on the usb beforehand as a check for the usb
-if [[ -e /run/media/nik/rhelscripts/neofetch-7.1.0/test.txt ]]; then
+if [[ -e /run/media/nik/rhelscripts/RHEL8/neofetch-7.1.0/test.txt ]]; then
   echo "test file exists on usb"
   echo "...Copying files from usb to Nik's home dir."
-  sudo cp -R /run/media/nik/rhelscripts/neofetch-7.1.0/ /home/nik
+  sudo cp -R /run/media/nik/rhelscripts/RHEL8/neofetch-7.1.0/ /home/nik
   echo "...Changing directories now to home/nik/neofetch-7.1.0/"
   cd /home/nik/neofetch-7.1.0/
   echo "running 'make install'"
@@ -19,13 +14,17 @@ if [[ -e /run/media/nik/rhelscripts/neofetch-7.1.0/test.txt ]]; then
   cd /home/nik
   echo "...Cleaning up neofetch directory."
   sudo rm -r home/nik/neofetch-7.1.0/
-  echo "neofetch" >> /home/nik/.bashrc
+  echo 'neofetch' | sudo tee -a /home/nik/.bashrc
   echo "...Copying files from usb to Nik's picture dir."
-  sudo cp /run/media/nik/rhelscripts/*.jpg /home/nik/Pictures
+  sudo cp /run/media/nik/rhelscripts/RHEL8/*.jpg /home/nik/Pictures
   echo "...Downloading and installing EPEL8"
   sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
   echo "...Enabling the codeready-builder-for-rhel-8-*-rpms repository"
   sudo subscription-manager repos --enable "codeready-builder-for-rhel-8-$(arch)-rpms"
+  echo ...Installing "make"
+  sudo yum install make -y
+  echo ...Installing "automake"
+  sudo yum install automake -y
   echo "...Installing NTFS-3G"
   sudo yum install ntfs-3g -y
   echo "...Installing Nmap"
@@ -34,8 +33,6 @@ if [[ -e /run/media/nik/rhelscripts/neofetch-7.1.0/test.txt ]]; then
   sudo yum install transmission -y
   echo "...Installing tor"
   sudo yum install tor -y
-  echo "...Installing keepassxc"
-  sudo yum install keepassxc -y
   echo "...installing libreoffice-writer "
   sudo yum install libreoffice-writer -y
   echo "...Installing Rust"
